7a96c6576a992d809fd3e65e65c9f3471c2924c4
Paulo Barros <paulo.barros@indicium.tech>
2025-08-07 13:27:44 -0300

    style: corrige formatação dos arquivos terraform (via fmt)

--- infra/terraform/envs/dev/provider.tf
+++ infra/terraform/envs/dev/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region  = var.region
-  profile = "default"  # ou o nome do perfil do ~/.aws/credentials
+  profile = "default"
 }
--- infra/terraform/modules/s3/main.tf
+++ infra/terraform/modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "this" {
-  bucket = "${var.bucket_name}"
+  bucket        = var.bucket_name
   force_destroy = true
 
   tags = {
