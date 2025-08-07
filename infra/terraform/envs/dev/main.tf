82fc6dc705a84aaf63ae64cafe4ae3e8d3538e8e
Paulo Barros <paulo.barros@indicium.tech>
2025-08-07 12:05:51 -0300

    chore: trigger terraform workflow

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -1,10 +1,5 @@
-provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
-}
-
 module "s3" {
   source      = "../../modules/s3"
   bucket_name = "poc-dados-dev-raw"
   environment = "dev"
-}
+}# ajuste para acionar o workflow
