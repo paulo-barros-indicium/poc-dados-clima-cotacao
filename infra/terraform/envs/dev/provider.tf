3dcd5cc9f3cbc3361a9966b8b8d08c5f9f967af5
Paulo Barros <paulo.barros@indicium.tech>
2025-08-07 18:03:48 -0300

    feat: Ajuste de endereco de credenciais

--- infra/terraform/envs/dev/provider.tf
+++ infra/terraform/envs/dev/provider.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 provider "google" {
-  credentials = file("../../gcp/credentials.json")
+  credentials = file("../../../gcp/credentials.json")
   project     = "poc-clima-cotacao"
 }
