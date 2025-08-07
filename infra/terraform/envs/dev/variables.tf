2cdd60d65bdcdd6585f83e1d4313546021089b6a
Paulo Barros <paulo.barros@indicium.tech>
2025-08-07 20:19:16 -0300

    feat: Credencias do GCP por variavel no Git

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -5,3 +5,9 @@ variable "project_name" {
 variable "region" {
   default = "us-east-1"
 }
+
+variable "gcp_credentials" {
+  type      = string
+  sensitive = true
+  description = "Credenciais do GCP no formato JSON compactado, injetadas via GitHub Secrets"
+}
